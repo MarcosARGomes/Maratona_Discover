b030bb191aa6f97654721459d9ce8227907e02b7
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-02-15 17:02:21 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -19,3 +19,59 @@ const Modal = {
     }
 }
 
+const transactions = [
+    { 
+        id: 1,
+        description: 'Luz',
+        amount:-50000,
+        date: '23/01/2021',
+     
+    },
+    {    id: 2,
+        description: 'Crição website',
+        amount:500000,
+        date: '23/01/2021',
+
+    }
+    {    id: 3,
+        description: 'Internet',
+        amount:-20000,
+        date: '23/01/2021',
+
+    }
+
+]
+
+const Transaction = {
+    incomes(){
+        //somar as entradas
+    },
+
+    expenses(){
+        //somar as saídas
+    },
+
+    total(){
+        // entradas - Saídas
+
+    }
+    
+
+}
+
+
+const DOM = {
+    innerHTMLTransaction(){
+
+        const html = `
+        <tr>
+            <td class="description">Luz</td>
+            <td  class="expenses">- R$ 500,00</td>
+            <td class="date">23/01/2021</td>
+            <td>
+                <img src="assets/minus.svg" alt="Remover Transação">
+             </td>
+        </tr>
+        `
+    }
+}
